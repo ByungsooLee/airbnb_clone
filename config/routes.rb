25a5d45c8c9ce472843ffa2de937f8de5b127a90
ByungsooLee <workout4050@gmail.com>
2019-11-04 11:22:00 +0800

    [layout] Create user show page

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
               path:"",
               controllers:{ registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
 end
