402c68567ccccec01902e1154b9d38ec36e2ec45
ByungsooLee <41956738+ByungsooLee@users.noreply.github.com>
2019-11-13 12:46:12 +0800

    [Function] Publish Room (#20)

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,11 @@
 module RoomsHelper
+  def is_done?(item)
+    unless item.blank?
+      content_tag :i, nil, class: "fa fa-check"
+    end
+  end
+
+  def is_ready?(room)
+    !room.price.blank? && !room.description.blank? && !room.photos.blank? && !room.location.blank?
+  end
 end
