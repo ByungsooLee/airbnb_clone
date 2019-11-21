ce27eb9dc9166b851a5c37711329a3bf8faa903e
ByungsooLee <41956738+ByungsooLee@users.noreply.github.com>
2019-11-21 11:21:12 +0800

    [Function] ReservationC (#25)

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,11 @@ Rails.application.routes.draw do
       get :photos
       get :amenities
       get :location
+      get :preload
+      get :preview
     end
     resources :photos, only: [:create, :destroy]
+    resources :reservations, only: [:create]
   end
   devise_for :users,
               path:"",
